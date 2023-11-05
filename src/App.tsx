cb99382beefc92060a7831ef018b0c6c6a391d26
jessica <jessicassoarez@gmail.com>
2023-11-05 16:16:46 -0300

    feat(configuration): configuration of esl to pathern the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,42 @@
-import { useState } from 'react'
+import  { useState } from 'react'
+
+import Header from '@/components/Header'
+
 import reactLogo from './assets/react.svg'
+
 import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [count, setCount] = useState(0)
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
+        <Header />
+        <a href="https://vitejs.dev" 
+          target="_blank" rel="noreferrer">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
+        <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div
+       className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is 
+          {' '}
+          {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Edit 
+          {' '}
+          <code>src/App.tsx</code>
+          {' '}
+          and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
@@ -33,3 +47,4 @@ function App() {
 }
 
 export default App
+
